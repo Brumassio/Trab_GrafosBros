8793f3675054fd76234bc0f0224d9ad4ccdf65de
Brumassio <dibrunspeed@gmail.com>
2022-03-09 00:53:31 -0300

    Busca em largura atualizada

--- src/Bfs.java
+++ src/Bfs.java
@@ -5,28 +5,38 @@ public class  Bfs {
 
     public static void buscaemlar(Grafos grafo, Vertice ver)
     {
-        for(Vertice u : grafo.vertices)
+        int i,j;
+        for(i=0; i< grafo.vertices.size(); i++/*Vertice u : grafo.vertices*/)
         {
-            ver.dzin = 0;
-            ver.pai = null;
-            ver.cor = "branco";
+            grafo.vertices.get(i).dzin = 0;
+            grafo.vertices.get(i).pai = null;
+            grafo.vertices.get(i).cor = "branco";
+            System.out.println("dentro do for do bfs");
         }
         ver.dzin = 0;
         ver.pai = null;
         ver.cor = "cinza";
+        System.out.println("oi");
         LinkedList<Vertice> queue = new LinkedList<Vertice>();
         queue.add(ver);
-        while(queue.size() != 0)
+        System.out.println("oi2, size queue: "+ queue.size());
+        //grafo.vertices.indexOf(u);
+        while(!queue.isEmpty())
         {
+            System.out.println("oi3");
             ver = queue.remove();
-            for(Vertice v : grafo.vertices)
+            System.out.println("oi4, valor de i : "+ i);
+            for(j=0; j < grafo.vertices.get(i).adj.size() ;j++/*Vertice v : grafo.vertices.get(index).adj*/)
             {
-                if(v.cor == "branco")
+                System.out.println("oi5");
+                if(grafo.vertices.get(i).adj.get(j).cor == "branco")
                 {
-                    v.dzin = ver.dzin +1;
-                    v.pai = ver;
-                    v.cor = "cinza";
-                    queue.add(v);
+                    System.out.println("oi6");
+                    grafo.vertices.get(i).adj.get(j).dzin = ver.dzin +1;
+                    grafo.vertices.get(i).adj.get(j).pai = ver;
+                    grafo.vertices.get(i).adj.get(j).cor = "cinza";
+                    queue.add(grafo.vertices.get(i).adj.get(j));
+
                 }
                 ver.cor = "preto";
             }
