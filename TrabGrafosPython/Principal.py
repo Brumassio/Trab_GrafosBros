8e2f7a8f3bd8e106f73f8cd5a4c690a0e4c4c97b
Brumassio <dibrunspeed@gmail.com>
2022-03-29 23:37:53 -0300

    Classe Main/Principal <3

---
+++ TrabGrafosPython/Principal.py
@@ -0,0 +1,14 @@
+from Grafo import Grafo
+
+
+grafo = Grafo(5)
+grafo.addAresta(1,2)
+grafo.addAresta(1,2)
+grafo.addAresta(1,2)
+
+print(grafo.vertices[0].adj)
+print(grafo.vertices[1].adj)
+#print(grafo.vertices[0].adj[0].num)
+print(dir(int))
+
+
