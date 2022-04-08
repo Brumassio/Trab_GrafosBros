fa994129a160a5f9c90c25ec8eb8eb47f5a4010b
Brumassio <dibrunspeed@gmail.com>
2022-04-08 17:42:34 -0300

    classe principal atualizada !

--- TrabGrafosPython/Principal.py
+++ TrabGrafosPython/Principal.py
@@ -1,14 +1,18 @@
 from Grafo import Grafo
+import tools
 
 
 grafo = Grafo(5)
 grafo.addAresta(1,2)
-grafo.addAresta(1,2)
-grafo.addAresta(1,2)
+grafo.addAresta(1,3)
+grafo.addAresta(1,4)
+grafo.addAresta(1,5)
+grafo.addAresta(2,3)
 
 print(grafo.vertices[0].adj)
 print(grafo.vertices[1].adj)
-#print(grafo.vertices[0].adj[0].num)
-print(dir(int))
-
-
+print("\n")
+# print(len(grafo.vertices))
+#grafo.printarCores(grafo)
+tools.arrumaCores(grafo)
+grafo.printarCores()
