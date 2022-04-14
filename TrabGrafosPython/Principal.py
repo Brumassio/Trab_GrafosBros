2ad3a5cfd7b43fadc75d264c70e0872839277c13
Brumassio <dibrunspeed@gmail.com>
2022-04-14 18:28:29 -0300

    criando o grafo com um dos 60 arquivos .txt

--- TrabGrafosPython/Principal.py
+++ TrabGrafosPython/Principal.py
@@ -1,15 +1,17 @@
 from Grafo import Grafo
 import tools
 
-n = int(input("tamanho do grafo: "))
-grafo = Grafo(n) 
+# n = int(input("tamanho do grafo: "))
+# grafo = Grafo(n) 
 
-for i in range(n):
-    j = i+1
-    while(j < n):
-        print(i+1,j+1)
-        grafo.addAresta(i+1,j+1)
-        j += 1
+# for i in range(n):
+#     j = i+1
+#     while(j < n):
+#         print(i+1,j+1)
+#         grafo.addAresta(i+1,j+1)
+#         j += 1
+src = "C:\\Users\\Diogo Brumassio\\Desktop\\Trabalho_Grafos\\Trabalho_GrafosBrosMin\\TrabGrafosPython\\grafos_de_entrada\\0_grafo.txt"
+grafo = tools.lerArquivo(src)
 # grafo.addAresta(1,2)
 # grafo.addAresta(1,3)
 # grafo.addAresta(1,4)
@@ -21,16 +23,17 @@ for i in range(n):
 # grafo.addAresta(3,5)
 # grafo.addAresta(4,5)
 
+grafo.printarCores()
 
 
-
-print(grafo.vertices[0].adj)
-print(grafo.vertices[1].adj)
-print("\n")
+# print(grafo.vertices[0].adj)
+# print(grafo.vertices[1].adj)
+# print("\n")
 # print(len(grafo.vertices))
 #grafo.printarCores(grafo)
-if tools.colorir(grafo):
-    print("Foi possível colorir o grafo !\nGrafo ficou:")
-    grafo.printarCores()
-else:
-    print("Não e possível colorir o grafo  com  4 cores")
+
+# if tools.colorir(grafo):
+#     print("Foi possível colorir o grafo !\nGrafo ficou:")
+#     grafo.printarCores()
+# else:
+#     print("Não e possível colorir o grafo  com  4 cores")
