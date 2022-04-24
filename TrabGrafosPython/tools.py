6dddd2208d35636c6afb83c26b39f575d10a25c2
Brumassio <dibrunspeed@gmail.com>
2022-04-24 13:12:36 -0300

    file tools sem prints !

--- TrabGrafosPython/tools.py
+++ TrabGrafosPython/tools.py
@@ -10,15 +10,14 @@ def colorir(grafo):
         if(grafo.vertices[i].cor != grafo.cores[len(grafo.cores)-1] ):
             grafo.trocarCor(grafo.vertices[i])
             if taColorido(grafo):
-                print(iteracoes)
+                print("Iteracoes: ",iteracoes)
                 return True
             i = 0
             iteracoes += 1
-            #print(iteracoes)
         else:
             grafo.vertices[i].cor = grafo.cores[0]
             i += 1
-    print(iteracoes)
+    print("Iteracoes: ",iteracoes)
     return False
        
 def taColorido(grafo):
@@ -32,20 +31,13 @@ def lerArquivo(nomeArq):
 
     with open(nomeArq,"r") as arquivo:
         linhas = arquivo.readlines()
-        print("ola:",linhas)
         listinha=[]
         for i in linhas:
             listinha.append( i.replace('\n', ""))
-        print("ola2:",listinha)
         grafo = Grafo(int(listinha[2].split("=")[1]))
         listinha = listinha[4:]
-        print("lista fofa: ",listinha)
         for i in listinha:
-            print(i)
             i = i.split(" ")
-            print("pos split",i)
-            print(f"i[0]: {i[0]} e i[1]: {i[1]}")
             grafo.addAresta(int(i[0]), int(i[1]))
-            print("oi")
     return grafo
         
